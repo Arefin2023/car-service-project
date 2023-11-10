cb1886a182084d3001762323de4ec9227268e996
Arefin2023 <arefin.hasnat@gmail.com>
2023-11-10 09:54:59 +0100

    basic setup of welcome page and routing

---
+++ web/src/pages/AppointmentPage.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export function AppointmentPage() {
+  return (
+    <>
+      <h3>Appointments page</h3>
+      <Link to="/welcome">Back to Welcome Page</Link>
+    </>
+  );
+}
