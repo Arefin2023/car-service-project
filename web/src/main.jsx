5106861e56d908dc1090852d983b9c4f360dda1d
Arefin2023 <arefin.hasnat@gmail.com>
2023-11-21 15:20:12 +0100

    integrate clerk authentication in web

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -11,6 +11,21 @@ import { AppointmentPage } from "./pages/AppointmentPage.jsx";
 import { WipPage } from "./pages/WipPage.jsx";
 import { MessagePage } from "./pages/MessagePage.jsx";
 import { ServiceHistoryPage } from "./pages/ServiceHistoryPage.jsx";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton,
+} from "@clerk/clerk-react";
+
+if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 const defaultTheme = createTheme();
 const router = createBrowserRouter([
   {
@@ -18,11 +33,84 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <LoginPage /> },
-      { path: "/welcome", element: <WelcomePage /> },
-      { path: "/appointment", element: <AppointmentPage /> },
-      { path: "/wip", element: <WipPage /> },
-      { path: "/message", element: <MessagePage /> },
-      { path: "/history", element: <ServiceHistoryPage /> },
+      {
+        path: "/sign-in/*",
+        element: <SignIn routing="path" path="/sign-in" />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUp routing="path" path="/sign-up" />,
+      },
+      {
+        path: "/welcome",
+        element: (
+          <>
+            <SignedIn>
+              <WelcomePage />
+              <UserButton afterSignOutUrl="http://localhost:5173/" />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        ),
+      },
+      {
+        path: "/appointment",
+        element: (
+          <>
+            <SignedIn>
+              <AppointmentPage />
+              <UserButton afterSignOutUrl="http://localhost:5173/" />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        ),
+      },
+      {
+        path: "/wip",
+        element: (
+          <>
+            <SignedIn>
+              <WipPage />
+              <UserButton afterSignOutUrl="http://localhost:5173/" />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        ),
+      },
+      {
+        path: "/message",
+        element: (
+          <>
+            <SignedIn>
+              <MessagePage />
+              <UserButton afterSignOutUrl="http://localhost:5173/" />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        ),
+      },
+      {
+        path: "/history",
+        element: (
+          <>
+            <SignedIn>
+              <ServiceHistoryPage />
+              <UserButton afterSignOutUrl="http://localhost:5173/" />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        ),
+      },
     ],
   },
 ]);
@@ -30,7 +118,12 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <RouterProvider router={router} />
+      <ClerkProvider
+        publishableKey={clerkPubKey}
+        signInUrl="http://localhost:5173/sign-in"
+      >
+        <RouterProvider router={router} />
+      </ClerkProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
