5b14e498ac5c500adb7b54802cc24235778ab095
Arefin2023 <arefin.hasnat@gmail.com>
2023-11-22 10:34:33 +0100

    wip expo authentication

---
+++ app/app/(tabs)/_layout.js
@@ -0,0 +1,54 @@
+import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import { useUser } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
+import { palette } from "../../styles/styles";
+export default function TabLayout() {
+  const { isSignedIn, isLoaded } = useUser();
+  if (!isLoaded) {
+    return <View style={{ flex: 1, backgroundColor: palette.darkBlue }}></View>;
+  }
+  if (!isSignedIn) {
+    return <Redirect href="/login" />;
+  }
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: () => {
+            return <FontAwesome name="home" size={24} color="blue" />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="appointment"
+        options={{
+          title: "Appointment",
+          tabBarIcon: () => {
+            return <FontAwesome name="car" size={24} color="green" />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: () => {
+            return <FontAwesome name="folder" size={24} color="orange" />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="message"
+        options={{
+          title: "Messages",
+          tabBarIcon: () => {
+            return <FontAwesome name="envelope" size={24} color="lightblue" />;
+          },
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/app/_layout.js
+++ app/app/_layout.js
@@ -1,44 +1,11 @@
-import { Tabs } from "expo-router";
-import { FontAwesome } from "@expo/vector-icons";
-export default function TabLayout() {
+import { Slot } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+export default function AppLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: () => {
-            return <FontAwesome name="home" size={24} color="blue" />;
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="appointment"
-        options={{
-          title: "Appointment",
-          tabBarIcon: () => {
-            return <FontAwesome name="car" size={24} color="green" />;
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="history"
-        options={{
-          title: "History",
-          tabBarIcon: () => {
-            return <FontAwesome name="folder" size={24} color="orange" />;
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="message"
-        options={{
-          title: "Messages",
-          tabBarIcon: () => {
-            return <FontAwesome name="envelope" size={24} color="lightblue" />;
-          },
-        }}
-      />
-    </Tabs>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <Slot />
+    </ClerkProvider>
   );
 }
