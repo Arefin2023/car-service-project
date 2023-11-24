a2963810345f8cd111c6410826c269842e5f4143
Arefin2023 <arefin.hasnat@gmail.com>
2023-11-24 12:45:19 +0100

    wip: appointment calendar

--- app/app/appointment.js
+++ app/app/appointment.js
@@ -1,10 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
 import { Link } from "expo-router";
-
+import { Calendar } from "react-native-calendars";
+import { useState } from "react";
 export default function Appointment() {
+  const [appointmentDate, setAppointmentDate] = useState("");
   return (
     <View>
       <Text>Book an Appointment</Text>
+      <View>
+        <Calendar
+          onDayPress={(day) => {
+            setAppointmentDate(day);
+          }}
+        />
+      </View>
+      {appointmentDate !== "" ? (
+        <View>
+          <Text>
+            You have selected an appointment for {appointmentDate.day}.
+            {appointmentDate.month}.{appointmentDate.year}
+          </Text>
+          <Pressable
+            style={{
+              backgroundColor: "steelblue",
+              width: 100,
+              borderRadius: 5,
+              height: 30,
+            }}
+          >
+            <Text style={{ textAlign: "center" }}>Click to confirm</Text>
+          </Pressable>
+        </View>
+      ) : null}
+
       <Link href="/">Home</Link>
       <Link href="/message">Messages</Link>
 
