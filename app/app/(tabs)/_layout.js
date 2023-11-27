5b50ea031b089d46f8f2200a976f43185f45da73
Ralf Siewert <ralf@actyvy.st>
2023-11-27 12:11:03 +0100

    add routing for profile page

--- app/app/(tabs)/_layout.js
+++ app/app/(tabs)/_layout.js
@@ -4,14 +4,39 @@ import { Tabs, Redirect } from "expo-router";
 import { View } from "react-native";
 
 import { palette } from "../styles/styles";
+import { useEffect, useState } from "react";
 export default function TabLayout() {
-  const { isSignedIn, isLoaded } = useUser();
+  const { isSignedIn, isLoaded, user } = useUser();
+  const [profileCompleted, setProfileCompleted] = useState(-1); // -1: not loaded, 0: not completed, 1: completed
+
+  useEffect(() => {
+    setProfileCompleted(-1);
+    async function checkProfile() {
+      // do axios call to check if profile is completed
+      // if completed, setProfileCompleted(1)
+      // else setProfileCompleted(0)
+      setProfileCompleted(0);
+    }
+    if (user?.id) {
+      checkProfile();
+    }
+  }, [user]);
+
   if (!isLoaded) {
+    console.log("loading", isLoaded, profileCompleted);
     return <View style={{ flex: 1, backgroundColor: palette.darkBlue }} />;
   }
   if (!isSignedIn) {
     return <Redirect href="/login" />;
   }
+  if (profileCompleted === -1) {
+    console.log("loading", isLoaded, profileCompleted);
+    return <View style={{ flex: 1, backgroundColor: palette.darkBlue }} />;
+  }
+  if (profileCompleted < 1) {
+    return <Redirect href="/profileform" />;
+  }
+
   return (
     <Tabs>
       <Tabs.Screen
