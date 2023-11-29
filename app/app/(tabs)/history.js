82a5753b8607b537fd0be252892788a8a0676f20
Arefin2023 <arefin.hasnat@gmail.com>
2023-11-29 16:06:52 +0100

    wip history data loading

--- app/app/(tabs)/history.js
+++ app/app/(tabs)/history.js
@@ -1,7 +1,28 @@
+import { useAuth } from "@clerk/clerk-expo";
+import axios from "axios";
 import { Link } from "expo-router";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
+const apiHost = process.env.EXPO_PUBLIC_API_HOST;
+
 export default function History() {
+  const { getToken } = useAuth();
+  useEffect(() => {
+    async function loadData() {
+      const url = `${apiHost}/profile/appointments`;
+      try {
+        const data = await axios.get(url, {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        });
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    loadData();
+  }, []);
+
   return (
     <View>
       <Text>Service History and reviews</Text>
