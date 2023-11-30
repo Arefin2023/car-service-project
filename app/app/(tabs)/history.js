8431c1c2fdb8f612eaf09a9d52de4ef15416da86
Arefin2023 <arefin.hasnat@gmail.com>
2023-11-30 13:01:28 +0100

    wip app-history data loading and web-styling

--- app/app/(tabs)/history.js
+++ app/app/(tabs)/history.js
@@ -1,14 +1,13 @@
 import { useAuth } from "@clerk/clerk-expo";
 import axios from "axios";
-import { Link } from "expo-router";
-import { useEffect } from "react";
+import { Link, useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
 import { Text, View } from "react-native";
-import { useFocusEffect } from "expo-router";
-import { useCallback } from "react";
 
 const apiHost = process.env.EXPO_PUBLIC_API_HOST;
 
 export default function History() {
+  const [serviceData, setServiceData] = useState("");
   const { getToken } = useAuth();
 
   useFocusEffect(
@@ -20,6 +19,7 @@ export default function History() {
             headers: { Authorization: `Bearer ${await getToken()}` },
           });
           // console.log(data);
+          setServiceData(data);
           console.log("history");
           console.log(data);
         } catch (error) {
@@ -30,26 +30,12 @@ export default function History() {
     }, [])
   );
 
-  // useEffect(() => {
-  //   async function loadData() {
-  //     const url = `${apiHost}/profile/appointments`;
-  //     try {
-  //       const { data } = await axios.get(url, {
-  //         headers: { Authorization: `Bearer ${await getToken()}` },
-  //       });
-  //       // console.log(data);
-  //       console.log("history");
-  //       console.log(data);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-  //   loadData();
-  // }, []);
-
   return (
     <View>
       <Text>Service History and reviews</Text>
+      <View>
+        <Text>{serviceData[1].service}</Text>
+      </View>
       <Link href="/">Home</Link>
       <Link href="/message">Messages</Link>
 
